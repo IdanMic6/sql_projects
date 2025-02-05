7016260528083cb1826fea3b859920a536219bb0
IdanMic6 <idan.mic6@gmail.com>
2025-02-05 15:19:45 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-def getOnlyNumber(message):
-    while True:
-        user_input = input("Please enter " + message)
-        if user_input.isdigit() and int(user_input) >= 0:
-            return int(user_input)  
-        else:
-            print("Your select: " + user_input + " - should be a non-negative number. Please try again.")
